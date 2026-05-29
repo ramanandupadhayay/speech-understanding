a7040a52be04207b46fd6597c777d314b7e6ade0
ramanandupadhayay <ramanandupadhayay615@gmail.com>
2026-05-29 13:18:01 +0900

    homework6 committed

---
+++ homework6.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def minimum_Fs(f):
+    
+    Fs = 2 * f
+    return Fs
+
+def omega(f, Fs):
+   
+    omega = 2 * np.pi * f / Fs
+    return omega
+
+def pure_tone(omega, N):
+    
+    n = np.arange(N)
+    x = np.cos(omega * n)
+    return x
